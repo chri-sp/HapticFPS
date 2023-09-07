e80f2d6f7440f35d59afb9e44921483ccd5c861c
chri-sp <47659802+chri-sp@users.noreply.github.com>
2023-09-07 16:48:41 +0200

    Aggiunto commenti con spiegazione significato parametri per le funzioni di feedback aptico

--- Assets/Scripts/Falcon.cs
+++ Assets/Scripts/Falcon.cs
@@ -72,7 +72,10 @@ public class Falcon : MonoBehaviour {
 	public static extern void RemoveSimpleForces();
 
 	// Viscosities
-
+	// c: Damping coefficient
+	// w: Heuristic weighting factor in the range 0 to 1 used to interpolate
+	//    between the previous viscous force and the current viscous force to reduce vibration. 
+	//    A value of 0 will give no weight to the current viscous force, and 1 will give full weight.
 	[DllImport ("FalconUnityPlugin")]
 	public static extern int AddViscosity(float c, float w);
 
@@ -86,7 +89,10 @@ public class Falcon : MonoBehaviour {
 	public static extern void RemoveViscosities();
 
 	// Surfaces
-	
+	// p: Surface contact point. Using the center of the haptic proxy results in a dilation of the surface by the proxy radius.
+	// n: Surface normal
+	// k: Spring constant
+	// c: Damping coefficient
 	[DllImport ("FalconUnityPlugin")]
 	public static extern int AddSurface(Vector3 p, Vector3 n, float k, float c);
 		
@@ -100,7 +106,11 @@ public class Falcon : MonoBehaviour {
 	public static extern void RemoveSurfaces();
 
 	// Springs
-	
+	// p: Position
+	// k: Spring constant
+	// c: Damping coefficient
+	// r: Rest length
+	// m: Maximum length. Negative value for no maximum.
 	[DllImport ("FalconUnityPlugin")]
 	public static extern int AddSpring(Vector3 p, float k, float c, float r, float m);
 
@@ -114,7 +124,23 @@ public class Falcon : MonoBehaviour {
 	public static extern void RemoveSprings();
 
 	// Intermolecular forces
-	
+	// Simulating using a linear spring with bond equilibrium length equal to rest length. Instead of breaking at maximum length, however, 
+	// the force curve is mirrored to achieve a continuous falloff, and thus a linear approximation of an intermolecular force curve:
+	//
+	// \
+	//  \
+	// __\_______
+	//   |\    /
+	//   | \  /
+	//   |  \/
+	//   |   | 
+	//   r   m
+	//
+	// p: Position
+	// k: Spring constant
+	// c: Damping coefficient
+	// r: Bond length
+	// m: Maximum length, beyond which bond is "broken"
 	[DllImport ("FalconUnityPlugin")]
 	public static extern int AddIntermolecularForce(Vector3 p, float k, float c, float r, float m);
 	
@@ -128,7 +154,10 @@ public class Falcon : MonoBehaviour {
 	public static extern void RemoveIntermolecularForces();
 
 	// Random forces
-		
+	// minMag: Minimum force magnitude
+	// maxMag: Maximum force magnitude
+	// minTime: Minimum time interval to apply force
+	// maxTime: Maximum time interval to apply force
 	[DllImport ("FalconUnityPlugin")]
 	public static extern int AddRandomForce(float minMag, float maxMag, float minTime, float maxTime);
 	
