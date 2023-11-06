f9e9accddb9c078482e5dc731481ba5be1c5e084
chri-sp <47659802+chri-sp@users.noreply.github.com>
2023-11-06 13:25:02 +0100

    Aggiunto collider nemico

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -12,7 +12,7 @@ public class EnemyHealth : MonoBehaviour
        hitPoints -= damage;
         if (hitPoints <= 0)
         {
-            Destroy(gameObject);
+            Destroy(transform.root.gameObject);
         }
     }
 }
