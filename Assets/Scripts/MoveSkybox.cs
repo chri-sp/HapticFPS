e5376dbba839c3c229b68ca3064e1c13a69a9856
chri-sp <47659802+chri-sp@users.noreply.github.com>
2023-11-20 10:47:19 +0100

    Aggiunto skybox

---
+++ Assets/Scripts/MoveSkybox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSkybox : MonoBehaviour {
+
+    [SerializeField] private float rotationSpeed = 1.0f;
+    float curRot = 0;
+
+    void Start ()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curRot += rotationSpeed * Time.deltaTime;
+        curRot %= 360;
+        RenderSettings.skybox.SetFloat("_Rotation", curRot);
+    }
+}
