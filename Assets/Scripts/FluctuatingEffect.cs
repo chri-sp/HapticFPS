166a67a6e2c9d529a70bcdf8703683e911787178
chri-sp <47659802+chri-sp@users.noreply.github.com>
2023-12-30 19:04:03 +0100

    Importato backgroung scena

---
+++ Assets/Scripts/FluctuatingEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluctuatingEffect : MonoBehaviour
+{
+
+    public float velocitaFluttuazioneVerticale = 1.0f;
+    public float ampiezzaFluttuazioneVerticale = 1.0f;
+    public float velocitaFluttuazioneOrizzontale = 1.0f;
+    public float ampiezzaFluttuazioneOrizzontale = 1.0f;
+
+    private Vector3 posizioneIniziale;
+
+    void Start()
+    {
+        posizioneIniziale = transform.position;
+        ampiezzaFluttuazioneVerticale = Random.Range(ampiezzaFluttuazioneVerticale - .5f, ampiezzaFluttuazioneVerticale + .5f);
+        ampiezzaFluttuazioneOrizzontale = Random.Range(ampiezzaFluttuazioneOrizzontale - .5f, ampiezzaFluttuazioneOrizzontale + .5f);
+        velocitaFluttuazioneVerticale = Random.Range(velocitaFluttuazioneVerticale - .5f, velocitaFluttuazioneVerticale + .5f);
+        ampiezzaFluttuazioneOrizzontale = Random.Range(ampiezzaFluttuazioneOrizzontale - .5f, ampiezzaFluttuazioneOrizzontale + .5f);
+    }
+
+    void Update()
+    {
+        float movimentoFluttuanteVerticale = ampiezzaFluttuazioneVerticale * Mathf.Sin(Time.time * velocitaFluttuazioneVerticale);
+
+        float movimentoFluttuanteOrizzontale = ampiezzaFluttuazioneOrizzontale * Mathf.Cos(Time.time * velocitaFluttuazioneOrizzontale);
+
+        // Aggiorna la posizione dell'oggetto
+        transform.position = posizioneIniziale + new Vector3(movimentoFluttuanteOrizzontale, movimentoFluttuanteVerticale, 0);
+    }
+}
